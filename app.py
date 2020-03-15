7b07e952aecda7a8ed2ecfb3ea565285285adbce
Fabian Schober <reach@fabianschober.com>
2020-03-15 13:47:31 +0100

    started building logger

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import os
 from pathlib import Path
 from controllers import log
+import logging
 
 from controllers.ctrls import lct_controller
 from views.views import main_frame
@@ -9,6 +10,8 @@ from models.models import lct_voc
 
 
 if __name__ == '__main__':
+    print(dir(log))
+    log.info("WELCOME TO THE LANGUAGE CONSTRUCTION TOOL")
     root = tk.Tk()  # TKINTER OBJECT
     app = lct_controller(root,  start_up=True) # GUI CONTROLLER
 
